9707b1d1ae844fe1687029f1d5ef45177e5284e2
kovendhan06 <96494314+kovendhan06@users.noreply.github.com>
2022-09-01 14:58:06 +0530

    Add files via upload

---
+++ htmlform/script.js
@@ -0,0 +1,11 @@
+function foo()
+{
+  var res=document.getElementById("fname").value; 
+  console.log(res);
+  var res1=document.getElementById("mname").value;
+  console.log(res1);
+  var res2=document.getElementById("lname").value;
+  console.log(res2); 
+  var res3=document.getElementById("main").value; 
+  console.log(res3); 
+}
